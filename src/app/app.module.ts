9860a0cf166cc142f75a7715bc8045eb7d8ffd9c
Shahed Iqbal <shahed.iqbal@leighton.com>
2023-08-01 14:37:05 +0100

    added new component for add skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { AddSkillsComponent } from './add-skills/add-skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplaySkillsComponent
+    DisplaySkillsComponent,
+    AddSkillsComponent
   ],
   imports: [
     BrowserModule,
