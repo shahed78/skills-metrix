7de695e0178763dbabcdd8c417ce3857f9f0b6e7
Shahed Iqbal <shahed.iqbal@leighton.com>
2023-08-31 11:45:59 +0100

    added new partials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,13 +23,15 @@ import { MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
 import { HttpClientModule } from '@angular/common/http';
 import { SkillsService } from './shared/services/skills.service';
 import { MiltiSelectComponent } from './shared/partials/milti-select/milti-select.component';
+import { MiltiSelectFormBuilderComponent } from './shared/partials/milti-select-form-builder/milti-select-form-builder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DisplaySkillsComponent,
     AddSkillsComponent,
-    MiltiSelectComponent
+    MiltiSelectComponent,
+    MiltiSelectFormBuilderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/partials/milti-select-form-builder/milti-select-form-builder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-milti-select-form-builder',
+  templateUrl: './milti-select-form-builder.component.html',
+  styleUrls: ['./milti-select-form-builder.component.scss']
+})
+export class MiltiSelectFormBuilderComponent {
+
+}
