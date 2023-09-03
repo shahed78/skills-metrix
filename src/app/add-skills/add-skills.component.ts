68d9663bcdab1ad2fdf8f95fcedd2e1bf4a126cd
Shahed Iqbal <shahed.iqbal@leighton.com>
2023-09-03 20:23:52 +0100

    multi select refactor

--- src/app/add-skills/add-skills.component.ts
+++ src/app/add-skills/add-skills.component.ts
@@ -16,31 +16,31 @@ import { MatSelect } from '@angular/material/select';
 export class AddSkillsComponent implements OnInit {
 
   skillsForm: FormGroup;
-  skills: ISkill[] = []; 
+  skills: ISkill[] = [];
   public filteredSkillsMulti: ReplaySubject<ISkill[]> = new ReplaySubject<ISkill[]>(1);
   @ViewChild('multiSelect', { static: false }) multiSelect!: MatSelect;
   protected _onDestroy = new Subject<void>();
   skillsMultiFilterCtrlName: string = 'skillsMultiFilterCtrl';
-  
-  constructor (
-    private skillsService: SkillsService, 
-    private dialogRef: DialogRef<AddSkillsComponent>, 
+
+  constructor(
+    private skillsService: SkillsService,
+    private dialogRef: DialogRef<AddSkillsComponent>,
     private fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public dialogdata: any
-    ){
-      this.skillsForm = this.fb.group({
-        firstname: ['', Validators.required],
-        lastname: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        startdate: ['', Validators.required],
-        enddate: ['', Validators.required],
-        skillsMultiCtrl: [],
-        skillsMultiFilterCtrl: ['']
-      });
-    }
+  ) {
+    this.skillsForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      startdate: ['', Validators.required],
+      enddate: ['', Validators.required],
+      skillsMultiCtrl: [],
+      skillsMultiFilterCtrl: ['']
+    });
+  }
 
   ngOnInit() {
-    this.skillsService.getSkills().subscribe({  
+    this.skillsService.getSkills().subscribe({
       next: skills => {
         console.log('Successfully get skills: ', skills);
         this.skills = skills;
@@ -53,74 +53,74 @@ export class AddSkillsComponent implements OnInit {
             this.filterSkillsMulti();
           });
 
-      },  
+      },
       error: err => console.error('An error occurred', err)
     });
 
     this.skillsForm.patchValue(this.dialogdata);
-}
-
-// multi select
-protected filterSkillsMulti() {
-  console.log('filterBanksMulti', this.skills);
-  if (!this.skills) {
-    return;
   }
-  let search = this.skillsForm.get('skillsMultiFilterCtrl')?.value;
-  if (!search) {
-    this.filteredSkillsMulti.next(this.skills.slice());
-    return;
-  } else {
-    search = search.toLowerCase();
+
+  // multi select
+  protected filterSkillsMulti() {
+    console.log('filterBanksMulti', this.skills);
+    if (!this.skills) {
+      return;
+    }
+    let search = this.skillsForm.get('skillsMultiFilterCtrl')?.value;
+    if (!search) {
+      this.filteredSkillsMulti.next(this.skills.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    this.filteredSkillsMulti.next(
+      this.skills.filter(bank => bank.name.toLowerCase().indexOf(search) > -1)
+    );
   }
-  this.filteredSkillsMulti.next(
-    this.skills.filter(bank => bank.name.toLowerCase().indexOf(search) > -1)
-  );
-}
 
-ngOnDestroy() {
-  this._onDestroy.next();
-  this._onDestroy.complete();
-}
+  ngOnDestroy() {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
 
-ngAfterViewInit() {
-  this.setInitialValue();
-}
+  ngAfterViewInit() {
+    this.setInitialValue();
+  }
 
-protected setInitialValue() {
-  this.filteredSkillsMulti
-    .pipe(take(1), takeUntil(this._onDestroy))
-    .subscribe(() => {
-      this.multiSelect.compareWith = (a: ISkill, b: ISkill) => a && b && a.id === b.id;
-    });
-}
+  protected setInitialValue() {
+    this.filteredSkillsMulti
+      .pipe(take(1), takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.multiSelect.compareWith = (a: ISkill, b: ISkill) => a && b && a.id === b.id;
+      });
+  }
 
 
 
-onSubmit(): void {
+  onSubmit(): void {
     if (this.skillsForm.invalid) {
       // If the form is invalid, do not submit
       return;
     }
 
-    if(this.dialogdata){
+    if (this.dialogdata) {
 
-      this.skillsService.editSkills(this.dialogdata.id, this.skillsForm.value).subscribe({  
+      this.skillsService.editSkills(this.dialogdata.id, this.skillsForm.value).subscribe({
         next: response => {
           this.dialogRef.close();
-        },  
-        error: err => console.error('An error occurred', err),  
-        complete: () => console.log('complete')  
+        },
+        error: err => console.error('An error occurred', err),
+        complete: () => console.log('complete')
       });
 
-    }else{
-      this.skillsService.addSkills(this.skillsForm.value).subscribe({  
+    } else {
+      this.skillsService.addSkills(this.skillsForm.value).subscribe({
         next: response => {
           console.log('Successfully added: ', response);
           this.dialogRef.close();
-        },  
-        error: err => console.error('An error occurred', err),  
-        complete: () => console.log('complete')  
+        },
+        error: err => console.error('An error occurred', err),
+        complete: () => console.log('complete')
       });
     }
 
