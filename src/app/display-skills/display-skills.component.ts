e4f827aaa897524681601a0aa6a1a9fd2c92a2e6
Shahed Iqbal <shahed.iqbal@leighton.com>
2023-09-27 20:26:08 +0100

    refactor

--- src/app/display-skills/display-skills.component.ts
+++ src/app/display-skills/display-skills.component.ts
@@ -27,149 +27,146 @@ export class DisplaySkillsComponent implements OnInit {
 
   @ViewChild(MatPaginator) paginator : MatPaginator;
 
-   
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    constructor(public dialog: MatDialog, private skillsService: SkillsService, private datePipe: DatePipe ) {}
 
-    if (this.dataSource.filterPredicate) {
-        this.dataSource.filterPredicate = (data: IUser, filter: string) => this.tableFilter(data, filter);
+    ngOnInit(): void {
+      this.getUsers();
+      this.getSkills();
     }
-  }
 
-  constructor(public dialog: MatDialog, private skillsService: SkillsService, private datePipe: DatePipe ) {}
-
-  ngOnInit(): void {
-    this.getUsers();
-    this.getSkills();
-  }
-
-  private tableFilter(tableDdata: IUser, filter: string) {
- 
-      return tableDdata.name.toLowerCase().includes(filter) || 
-              tableDdata.email.toLowerCase().includes(filter) || 
-              tableDdata.location.toLowerCase().includes(filter) ||
-              tableDdata.role.toLowerCase().includes(filter) ||
-              this.formatSkills(tableDdata.skillsMultiCtrl).toLowerCase().includes(filter);
-  }
-
-
-  private dateToTransform(dateString: string) {
-    return  dateString ? this.datePipe.transform(new Date(dateString), 'dd/MM/yyyy HH:mm:ss') || '' : ''
-  }
-
-  public formatSkills(skillsMultiCtrl: ISkill[]): string { // type
-    return skillsMultiCtrl.map(skill => skill.name).join(', ');
-  }
-
-  private getUsers(): void {
-
-    this.skillsService.getUsers()
-    .subscribe({
-      next: userdata =>{
-        this.users = userdata.sort((a, b) => a.id - b.id);
-
-        if (this.paginator) {
-          this.dataSource = new MatTableDataSource(this.users);
-          this.dataSource.paginator = this.paginator; // Set the paginator
-          this.paginator.length = this.users.length; // Set the length property
+    public applyFilter(event: Event) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+  
+      if (this.dataSource.filterPredicate) {
           this.dataSource.filterPredicate = (data: IUser, filter: string) => this.tableFilter(data, filter);
+      }
+    }
+
+    private tableFilter(tableDdata: IUser, filter: string) {
+  
+        return tableDdata.name.toLowerCase().includes(filter) || 
+                tableDdata.email.toLowerCase().includes(filter) || 
+                tableDdata.location.toLowerCase().includes(filter) ||
+                tableDdata.role.toLowerCase().includes(filter) ||
+                this.formatSkills(tableDdata.skillsMultiCtrl).toLowerCase().includes(filter);
+    }
+
+
+    private dateToTransform(dateString: string) {
+      return  dateString ? this.datePipe.transform(new Date(dateString), 'dd/MM/yyyy HH:mm:ss') || '' : ''
+    }
+
+    public formatSkills(skillsMultiCtrl: ISkill[]): string { // type
+      return skillsMultiCtrl.map(skill => skill.name).join(', ');
+    }
+
+    private getUsers(): void {
+
+      this.skillsService.getUsers()
+      .subscribe({
+        next: userdata =>{
+          this.users = userdata.sort((a, b) => a.id - b.id);
+
+          if (this.paginator) {
+            this.dataSource = new MatTableDataSource(this.users);
+            this.dataSource.paginator = this.paginator; // Set the paginator
+            this.paginator.length = this.users.length; // Set the length property
+            this.dataSource.filterPredicate = (data: IUser, filter: string) => this.tableFilter(data, filter);
+          }
+        },
+        error: err => {
+          console.log(err);
         }
-      },
-      error: err => {
-        console.log(err);
-      }
-    });
-  }
+      });
+    }
 
-  private getSkills(): void {
-    this.skillsService.getSkills().subscribe({
-      next: skills => {
-        this.skills = skills;
+    private getSkills(): void {
+      this.skillsService.getSkills().subscribe({
+        next: skills => {
+          this.skills = skills;
 
-      },
-      error: err => console.error('An error occurred', err)
-    });
-  }
+        },
+        error: err => console.error('An error occurred', err)
+      });
+    }
 
-  public addUser(): void {
-    //name
-    const dialogRef = this.dialog.open(AddSkillsComponent);
-    dialogRef.afterClosed().subscribe(()=>this.getUsers());
-  }
+    public addUser(): void {
+      //name
+      const dialogRef = this.dialog.open(AddSkillsComponent);
+      dialogRef.afterClosed().subscribe(()=>this.getUsers());
+    }
 
-  public editUser(user: IUser): void {
-    //name
-    const dialogRef = this.dialog.open(AddSkillsComponent, {
-      data: user
-    });
-    //improve
-    dialogRef.afterClosed().subscribe(()=>this.getUsers());
-  }
+    public editUser(user: IUser): void {
+      //name
+      const dialogRef = this.dialog.open(AddSkillsComponent, {
+        data: user
+      });
+      //improve
+      dialogRef.afterClosed().subscribe(()=>this.getUsers());
+    }
 
-  public deleteUser(id: number): void {
-    const dialogRef = this.dialog.open(DeleteConfirmationComponent, {
-      data: this.users.filter(t=> t.id===id)[0]
-    });
-  
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result === true) {
-        // User confirmed deletion, perform the delete action
-        this.skillsService.deleteUser(id).subscribe({
-          next: () => {
-            this.getUsers();
-            this.skillsService.notification('Skill removed successfully');
-          },
-          error: (err) => {
-            console.log(err);
-            this.skillsService.notification('Failed to remove skill. Please try again later.');
-          },
-        });
-      }
-    });
-  }
+    public deleteUser(id: number): void {
+      const dialogRef = this.dialog.open(DeleteConfirmationComponent, {
+        data: this.users.filter(t=> t.id===id)[0]
+      });
+    
+      dialogRef.afterClosed().subscribe((result) => {
+        if (result === true) {
+          // User confirmed deletion, perform the delete action
+          this.skillsService.deleteUser(id).subscribe({
+            next: () => {
+              this.getUsers();
+              this.skillsService.notification('Skill removed successfully');
+            },
+            error: (err) => {
+              console.log(err);
+              this.skillsService.notification('Failed to remove skill. Please try again later.');
+            },
+          });
+        }
+      });
+    }
 
+    public openUploadDialog(): void {
+      const dialogRef = this.dialog.open(UploadSkillsComponent, {
+        width: '400px',
+        data: { users: this.users, skills: this.skills },
+      });
+    
+      dialogRef
+        .afterClosed()
+        .pipe(
+          concatMap(async (result: { convertedExcelData: IUser[], excelSkills: { name: string, type: string }[] }) => {
+            const { convertedExcelData, excelSkills } = result;
+    
+            this.showSpinner = true;
+    
+            if (excelSkills) {
+              console.log('skills');
+              await this.addExcelSkills(excelSkills);
+            }
+    
+            if (convertedExcelData ) {
+              const newUserToInsert = convertedExcelData.filter(user1 => !this.users.some(user2 => user2.id === user1.id));
+              const updateNewExcelDataRecordDifference = convertedExcelData.filter(item2 =>
+                this.users.some(item1 => item1.id === item2.id && this.isUserDataDifferent(item1, item2))
+              );
+              console.log(newUserToInsert);
+              console.log(updateNewExcelDataRecordDifference);
+    
+              await this.addExcelUser(newUserToInsert, updateNewExcelDataRecordDifference);
+            }
+    
+            this.showSpinner = false;
+          })
+        )
+        .subscribe();
+    }
 
-
-  public openUploadDialog(): void {
-    const dialogRef = this.dialog.open(UploadSkillsComponent, {
-      width: '400px',
-      data: { users: this.users, skills: this.skills },
-    });
-  
-    dialogRef
-      .afterClosed()
-      .pipe(
-        concatMap(async (result: { convertedExcelData: IUser[], excelSkills: { name: string, type: string }[] }) => {
-          const { convertedExcelData, excelSkills } = result;
-  
-          this.showSpinner = true;
-  
-          if (excelSkills) {
-            console.log('skills');
-            await this.addExcelSkills(excelSkills);
-          }
-  
-          if (convertedExcelData ) {
-            const newUserToInsert = convertedExcelData.filter(user1 => !this.users.some(user2 => user2.id === user1.id));
-            const updateNewExcelDataRecordDifference = convertedExcelData.filter(item2 =>
-              this.users.some(item1 => item1.id === item2.id && this.isUserDataDifferent(item1, item2))
-            );
-            console.log(newUserToInsert);
-            console.log(updateNewExcelDataRecordDifference);
-  
-            await this.addExcelUser(newUserToInsert, updateNewExcelDataRecordDifference);
-          }
-  
-          this.showSpinner = false;
-        })
-      )
-      .subscribe();
-  }
-
-  private skillsToAdd(excelSkills: IKnowladge[]) {
-    return excelSkills.filter((excelSkill: IKnowladge) => !this.skills.some((skill: IKnowladge) => skill.name === excelSkill.name));
-  }
+    private skillsToAdd(excelSkills: IKnowladge[]) {
+      return excelSkills.filter((excelSkill: IKnowladge) => !this.skills.some((skill: IKnowladge) => skill.name === excelSkill.name));
+    }
 
     public async addExcelSkills(excelSkills: IKnowladge[]): Promise<void> {
       try {
@@ -194,7 +191,7 @@ export class DisplaySkillsComponent implements OnInit {
         await this.processInSequence(addUsers, this.addImportedUser.bind(this));
         this.getUsers();
         console.log('Addition task completed');
-       }
+        }
 
       if(editUsers.length > 0){
 
