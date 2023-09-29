55d123001c751628b38976f4e4d9d4372e6da758
Shahed Iqbal <shahed.iqbal@leighton.com>
2023-09-29 11:09:07 +0100

    commented code removed

--- src/app/display-skills/display-skills.component.ts
+++ src/app/display-skills/display-skills.component.ts
@@ -236,30 +236,6 @@ export class DisplaySkillsComponent implements OnInit {
     return true;
   }
 
-  // private async processInSequence<T>(items: T[], actionFunction: (item: T) => Promise<void>): Promise<void> {
-  //   const BATCH_SIZE = 5; // Adjust the batch size as needed
-  //   const DELAY_BETWEEN_BATCHES_MS = 2000; // Adjust the delay (in milliseconds) between batches as needed
-  //   try {
-  //     for (let i = 0; i < items.length; i += BATCH_SIZE) {
-  //       const batch = items.slice(i, i + BATCH_SIZE);
-  //       await this.processBatch(batch, actionFunction);
-  //       await new Promise(resolve => setTimeout(resolve, DELAY_BETWEEN_BATCHES_MS));
-  //     }
-  //   } catch (error) {
-  //     console.log('Error in processing sequence:', error);
-  //   }
-  // }
-
-  // private async processBatch<T>(items: T[], actionFunction: (item: T) => Promise<void>): Promise<void> {
-  //   try {
-  //     for (const item of items) {
-  //       await actionFunction(item);
-  //     }
-  //   } catch (error) {
-  //    console.log('error in processing batch')
-  //   }
-  // }
-
   private async addImportedUser(user: IUser): Promise<void> {
     try {
       await firstValueFrom(this.usersService.addUser(user));
